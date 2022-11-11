6c5f93881f18a7fa196813346481e9eabe128d45
zhang yue <yuedev.cn@gmail.com>
2022-11-11 15:27:05 +0800

    hello metal.draw a triangle.

--- MetalTest/ViewController.swift
+++ MetalTest/ViewController.swift
@@ -6,14 +6,150 @@
 //
 
 import UIKit
+import Metal
+import QuartzCore
+import Foundation
+
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private lazy var device = {
+        MTLCreateSystemDefaultDevice()
+    }()
+
+    private lazy var metalLayer = {
+        CAMetalLayer()
+    }()
+
+    private var vertexBuffer: MTLBuffer? = nil
+
+    private var renderPass: MTLRenderPassDescriptor? = nil
+
+    private var pipeLineState: MTLRenderPipelineState? = nil
+
+    private var commandQueue: MTLCommandQueue? = nil
+
+
+    override func viewSafeAreaInsetsDidChange() {
+        super.viewSafeAreaInsetsDidChange()
+        view.backgroundColor = .lightGray
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame.inset(by: view.safeAreaInsets)
+
+        view.layer.addSublayer(metalLayer)
+        do {
+            try readyForDraw()
+        } catch {
+            print(error)
+            return
+        }
+
+        startRender()
     }
 
+
+
+    private func startRender() {
+        //game loop 根据屏幕的刷新来更新画面
+        let timer = CADisplayLink(target: self, selector: #selector(gameLoop))
+        timer.add(to: RunLoop.main, forMode: .default)
+    }
+
+    //渲染 draw call
+
+    func render() {
+
+        guard let pipeLineState = pipeLineState,
+              let vertexBuffer = vertexBuffer,
+              let renderPass = renderPass
+        else {
+            return
+        }
+
+        guard let drawable = metalLayer.nextDrawable() else {
+            return
+        }
+        //render pass设置纹理
+        renderPass.colorAttachments[0].texture = drawable.texture
+
+        //生成command buffer 这里放到渲染外初始化不行
+        guard let commandBuffer = commandQueue?.makeCommandBuffer() else {
+            return
+        }
+
+
+        //生成command encoder
+        guard let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPass) else {
+            return
+        }
+
+        //设置renderEncoder 渲染编码
+        //设置pipeline state
+        renderEncoder.setRenderPipelineState(pipeLineState)
+        //发送顶点数据  这个好像没法和opengl一样在渲染之前设置
+        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        //绘制三角形，1个
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+        //endEncoding 结束渲染编码
+        renderEncoder.endEncoding()
+
+        //commandBuffer 设置drawable并提交
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+
+    @objc func gameLoop() {
+        autoreleasepool {
+            render()
+        }
+    }
+
+    //渲染之前的准备
+
+    private func readyForDraw() throws {
+
+        guard let device = self.device else {
+            return
+        }
+
+        //顶点数据
+        let vertexData: [Float] = [
+            0.0, 0.5, 0.0,
+            -0.5, -0.5, 0.0,
+            0.5, -0.5, 0.0,
+        ]
+
+        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
+
+        //生成顶点vertexBuffer
+        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [])
+
+        //生成shader program
+        guard let defaultLibrary = device.makeDefaultLibrary() else {
+            return
+        }
+
+        let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
+        let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
+
+        //根据pipeline的描述文件 生成pipeline state
+        let pipeLineDescriptor = MTLRenderPipelineDescriptor()
+        pipeLineDescriptor.vertexFunction = vertexProgram
+        pipeLineDescriptor.fragmentFunction = fragmentProgram
+        pipeLineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+
+        pipeLineState = try device.makeRenderPipelineState(descriptor: pipeLineDescriptor)
+
+        //生成命令队列 和 命令buffer
+        commandQueue = device.makeCommandQueue()
+
+        //render pass 可以设置color clear之类的
+        renderPass = MTLRenderPassDescriptor()
+        renderPass?.colorAttachments[0].loadAction = .clear
+        renderPass?.colorAttachments[0].clearColor = MTLClearColorMake(0.2, 0.4, 0.3, 1.0)
+    }
 
 }
 
