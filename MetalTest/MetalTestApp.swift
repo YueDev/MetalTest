ce9d410c2b66bb086a45f0e23624ea43fbeb32d9
zhang yue <yuedev.cn@gmail.com>
2022-12-12 10:56:59 +0800

    add swiftui.

---
+++ MetalTest/MetalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalTestApp.swift
+//  MetalTest
+//
+//  Created by YUE on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct MetalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
